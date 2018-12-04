1033ea826f36d3349bf8ee09ee3c474d45105063
LeTuan252 <lethanhtuan252@gmail.com>
2018-12-04 17:26:43 +0700

    error in header

--- src/main/java/demo/controller/MainController.java
+++ src/main/java/demo/controller/MainController.java
@@ -1,15 +1,11 @@
 package demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import demo.dao.OrderDAO;
 import demo.dao.ProductDAO;
@@ -19,7 +15,7 @@ import demo.validator.CustomerFormValidator;
 
 @Controller
 @Transactional
-public class AdminController {
+public class MainController {
 	@Autowired
 	private OrderDAO orderDAO;
 	@Autowired
@@ -40,22 +36,14 @@ public class AdminController {
 			dataBinder.setValidator(customerFormValidator);
 		}
 	}
-
-	   
-	@RequestMapping(value = { "/admin/login" }, method = RequestMethod.GET)
-    public String login(Model model) {
- 
-        return "login";
+	
+	@RequestMapping("/403")
+	public String accessDenied() {
+		return "/403";
+		}
+	@RequestMapping("/")
+    public String home() {
+        return "index";
     }
-	@RequestMapping(value = { "/admin/accountInfo" }, method = RequestMethod.GET)
-    public String accountInfo(Model model) {
-        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        System.out.println(userDetails.getUsername());
-        System.out.println(userDetails.getPassword());
-        System.out.println(userDetails.isEnabled());
-        model.addAttribute("userDetails", userDetails);
-        return "accountInfo";
-    }
-		
 	
 }
